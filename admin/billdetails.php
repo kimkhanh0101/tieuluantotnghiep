296ae49c81227d176a077ef2a105af36c626f21c
Van Dinh Tam HUYNH <hvdtam@gmail.com>
2023-04-29 21:09:45 +0700

    .

--- admin/billdetails.php
+++ admin/billdetails.php
@@ -3,46 +3,49 @@ include('includes/header.php');
 include('includes/navbar.php');
 
 ?>
-<?php include '../classes/brand.php'?>
-<?php include '../classes/category.php'?>
-<?php include '../classes/product.php'?>
-<?php include '../classes/bill.php'?>
+<?php include '../classes/brand.php' ?>
+<?php include '../classes/category.php' ?>
+<?php include '../classes/product.php' ?>
+<?php include '../classes/bill.php' ?>
 
 
 
 <?php
-  $fm = new Format();
-  $brand = new brand();
-  $cat = new category();
-  $prod = new product();
-  $bill = new bill();
+$fm    = new Format();
+$brand = new brand();
+$cat   = new category();
+$prod  = new product();
+$bill  = new bill();
 
-          if(isset($_POST["delete_id"])){
-          $id = $_POST["delete_id"];
-        $delbrand = $prod->delete_productName($id);
-          }
+if(isset($_POST["delete_id"])){
+    $id       = $_POST["delete_id"];
+    $delbrand = $prod->delete_productName($id);
+}
 
- ?>
+?>
 
 
 <style type="text/css">
-  .rows {
-display: flex;
-flex-wrap: wrap;
-margin-right: -.100rem;
-margin-left: -.75rem;
-}
-.scroll{
-  height: 400px;
-  overflow: scroll;
-}
-h4{
-  font-weight: bold;
-  color: red;
-}
-#top{
-  padding-top: 9px;
-}
+    .rows {
+        display: flex;
+        flex-wrap: wrap;
+        margin-right: -.100rem;
+        margin-left: -.75rem;
+    }
+
+    .scroll {
+        height: 400px;
+        overflow: scroll;
+    }
+
+    h4 {
+        font-weight: bold;
+        color: red;
+    }
+
+    #top {
+        padding-top: 9px;
+    }
 </style>
 
 <!-- /.container-fluid -->
@@ -50,126 +53,129 @@ h4{
 
 <?php
 
-  if (isset($_GET['idbill'])) {
-      $id_bill=$_GET['idbill'];
-    }
+if(isset($_GET['idbill'])){
+    $id_bill = $_GET['idbill'];
+}
 
 
-
-
- ?>
-
+?>
 
 
 <div class="container-fluid">
-<!-- DataTales Example -->
-<div class="card shadow mb-4">
-  <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary">BILL DETAILS
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">BILL DETAILS
 
-    </h6>
-     <form action="" method="POST">
+            </h6>
+            <form action="" method="POST">
 
-  </div>
-  <div class="card-body">
+        </div>
+        <div class="card-body">
 
-    <div class="table-responsive">
+            <div class="table-responsive">
 
-     <form action="" method="POST">
-      <h4>Thông tin người đặt hàng</h4>
+                <form action="" method="POST">
+                    <h4>Thông tin người đặt hàng</h4>
+                    <?php $billUser = mysqli_fetch_array($bill->get_BillUser($id_bill)) ?>
+                    <div class="col-12 container">
+                        <ul class="list-unstyled row">
+
+                            <li class="list-item col-6 border py-2">ID Đơn hàng</li>
+                            <li class="list-item col-6 border py-2"><?=$billUser[0]?></li>
+                            <li class="list-item col-6 border py-2">Người đặt</li>
+                            <li class="list-item col-6 border py-2"><?=$billUser[2]?></li>
+                            <li class="list-item col-6 border py-2">Người nhận</li>
+                            <li class="list-item col-6 border py-2"><?=$billUser[3]?></li>
+                            <li class="list-item col-6 border py-2">Số điện thoại người nhận</li>
+                            <li class="list-item col-6 border py-2"><?='0'.$billUser[4]?></li>
+                            <li class="list-item col-6 border py-2">Email người nhận</li>
+                            <li class="list-item col-6 border py-2"><?=$billUser[5]?></li>
+                            <li class="list-item col-6 border py-2">Địa chỉ</li>
+                            <li class="list-item col-6 border py-2"><?=$billUser[6]?></li>
+                            <li class="list-item col-6 border py-2">Thời gian đặt hàng</li>
+                            <li class="list-item col-6 border py-2"><?=$billUser[1]?></li>
+                            <li class="list-item col-6 border py-2">Tổng tiền</li>
+                            <li class="list-item col-6 border py-2"><?=$fm->format_currency($billUser[7])?></li>
+                        </ul>
+                    </div>
+
+                </form>
+
+            </div>
+
+        </div>
+
+        <div class="card-body">
+
+            <div class="table-responsive">
+
+                <form action="" method="POST">
+                    <h4>Thông tin đặt hàng</h4>
 
 
-      <div class="col-12 container">  
-      <ul class="list-unstyled row">
+                    <div class="scroll">
+                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                            <tr>
+                                <th> Tên sản phẩm</th>
+                                <th> Hình ảnh</th>
+                                <th> Kích thước</th>
+                                <th> Số lượng</th>
+                                <th> Số tiền</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php
 
-  <li class="list-item col-6 border py-2">ID Đơn hàng</li>
-  <li class="list-item col-6 border py-2">Người đặt</li>
-  <li class="list-item col-6 border py-2">Người nhận</li>
-  <li class="list-item col-6 border py-2">Số điện thoại người nhận</li>
-  <li class="list-item col-6 border py-2">Địa chỉ</li>
-  <li class="list-item col-6 border py-2">Thời gian đặt hàng</li>
-  <li class="list-item col-6 border py-2">Tổng tiền</li>
-</ul>
-          </div>
 
-       </form>
+                            $get_BillDetails = $bill->get_BillDetails($id_bill);
+                            if($get_BillDetails){
+                                while ($result = mysqli_fetch_array($get_BillDetails)) {
+
+
+                                    ?>
+                                    <tr>
+
+                                        <td> <?php echo $result['productName']; ?></td>
+                                        <td><img src="uploads/<?php echo $result['image'] ?>" width="70"></td>
+                                        <td> <?php echo $result['size']; ?></td>
+                                        <td> <?php echo $result['quantity']; ?></td>
+
+                                        <td><?php echo $fm->format_currency($result['price']) ?></td>
+                                    </tr>
+
+                                    <?php
+                                }
+                            }
+                            ?>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </form>
+
+            </div>
+
+        </div>
 
     </div>
-
-  </div>
-
-  <div class="card-body">
-
-    <div class="table-responsive">
-
-     <form action="" method="POST">
-      <h4>Thông tin đặt hàng</h4>
-
-
-      <div class="scroll">
-      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-        <thead>
-          <tr>
-            <?php echo var_dump($bill->get_BillUser($id_bill));?>
-            <th> Tên sản phẩm </th>
-            <th> Hình ảnh </th>
-            <th> Kích thước </th>
-            <th> Số lượng </th>
-            <th> Số tiền </th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-
-
-
-              $get_BillDetails=$bill->get_BillDetails($id_bill);
-              if ($get_BillDetails){
-                  while ($result=mysqli_fetch_array($get_BillDetails)) {
-
-
-             ?>
-          <tr>
-
-            <td> <?php echo $result['productName']; ?></td>
-            <td><img src="uploads/<?php echo $result['image']?>" width="70" ></td>
-            <td> <?php echo $result['size']; ?></td>
-            <td> <?php echo $result['quantity']; ?></td>
-
-             <td><?php echo  $fm->format_currency($result['price']) ?></td>
-           </tr>
-
-        <?php
-          }
-            }
-            ?>
-        </tbody>
-      </table>
-      </div>
-
-       </form>
-
-    </div>
-
-  </div>
-
-</div>
 
 </div>
 
 <script>
-    $(document).ready(function(){
-        $('#category').change(function(){
+    $(document).ready(function () {
+        $('#category').change(function () {
             var catName = $('#category option:selected').text();
             data = {
-                category:1,
-                catName:catName
+                category: 1,
+                catName: catName
             };
             $.ajax({
-                url:"size.php",
-                type:"POST",
-                data:data
-            }).done(function(result){
+                url: "size.php",
+                type: "POST",
+                data: data
+            }).done(function (result) {
                 $('#size').html(result);
 
             })
--- classes/bill.php
+++ classes/bill.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	$filepath = realpath(dirname(__FILE__));
 	include_once ($filepath.'/../lib/database.php');
 	include_once  ($filepath.'/../helpers/format.php');
@@ -7,10 +7,10 @@
 
 
 
-<?php 
+<?php
 
 	/**
-	 * 
+	 *
 	 */
 	class bill
 	{
@@ -23,8 +23,8 @@
 			$this->fm = new Format();
 		}
 				public function insert_Order($data,$buyerr){
-			
-			$name = mysqli_real_escape_string($this->db->link, $data['name']);	
+
+			$name = mysqli_real_escape_string($this->db->link, $data['name']);
 			$address = mysqli_real_escape_string($this->db->link, $data['address']);
 			$phone = mysqli_real_escape_string($this->db->link, $data['phone']);
 			$email = mysqli_real_escape_string($this->db->link, $data['email']);
@@ -41,13 +41,13 @@
 				}
 
 			}
-			
+
 			$query_order = "INSERT INTO tbl_order ( buyer, receiver, phone, email, address, totalprice,status) VALUES ('$buyerr','$name','$phone','$email','$address','$total','0')";
 			$insertOder = $this->db->insert($query_order);
 			return $insertOder;
 			}
-		
-		
+
+
 		public function get_Bill_by_Customer($cus){
 
 			$query = "SELECT * FROM tbl_order  WHERE buyer = '$cus' ORDER BY order_Id DESC";
@@ -64,7 +64,7 @@
 		public function delete_Order($id){
 			$query = "DELETE  FROM tbl_order WHERE order_Id = '$id' ";
 			$result = $this->db->delete($query);
-			return $result;		
+			return $result;
 		}
 
 		public function get_BillDetails($bill){
@@ -73,14 +73,14 @@
 			$result = $this->db->select($query);
 			return $result;
 		}
-		
+
 		public function get_BillUser($bill){
 
 			$query = "SELECT * FROM tbl_order WHERE order_Id =  '$bill'";
 			$result = $this->db->select($query);
 			return $result;
 		}
-		
+
 		public function get_Bill_by_Id($id){
 
 			$query = "SELECT a.order_Id, a.date , a.buyer, a.receiver, a.phone,a.email,a.totalprice,a.address, b.name as 'city', c.name as 'dis' FROM tbl_order a, tbl_city b, tbl_district c WHERE a.city=b.matp AND a.district=c.maqh AND order_Id = '$id'";
@@ -100,21 +100,21 @@
 					$price = $result['price'];
 					$query = "INSERT INTO tbl_orderdetails(id_order, productName, size, quantity, image, price) VALUES ('$MaxId','$productName','$size','$quantity','$image','$price')";
 					$insertOrderDetails = $this->db->insert($query);
-					
+
 
 				}
 			}
 			return $insertOrderDetails;
 		}
 		public function get_Bill_Max(){
-			
+
 			$query = "SELECT * FROM tbl_order WHERE order_Id= (SELECT max(order_Id) FROM tbl_order)";
-			
+
 			$get = $this->db->select($query);
 			return $get;
 		}
 		public function show_Discount(){
-			
+
 			$query = "SELECT * FROM tbl_discount ";
 			$result = $this->db->select($query);
 			if($result){
@@ -122,24 +122,24 @@
 			}
 		}
 		public function update_Status($status,$id){
-			
+
 			$id = mysqli_real_escape_string($this->db->link, $id);
 			$status = mysqli_real_escape_string($this->db->link, $status);
 
-			
+
 			$query = "UPDATE tbl_order SET status = '$status' WHERE order_Id = '$id'";
 			$result = $this->db->update($query);
 			if($result){
 				$alert = "<span class='text-danger' >Update status thành công</span";
 				return $alert;
-				
+
 
 			}
 			else{
 				$alert = "Lỗi. Update staus thất bại";
-				return $alert;	
+				return $alert;
 			}
-			
+
 		}
 		public function totalprice(){
 			$query = "SELECT SUM(totalprice)  as 'total' FROM tbl_order ";
@@ -166,7 +166,7 @@
 		public function deleteBill($id){
 			$query = "DELETE  FROM tbl_order WHERE order_Id = '$id' ";
 			$result = $this->db->delete($query);
-			return $result;		
+			return $result;
 		}
 		public function CountProduct(){
 			$query = "SELECT COUNT(quantity) as A FROM tbl_orderdetails";
